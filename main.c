5b114222128e321813f5c0952454231c596dfc23
David Robertson <dwrobertson1504@gmail.com>
2014-02-18 22:59:55 -0500

    Rewrote waterfallrun state

--- main.c
+++ main.c
@@ -10,15 +10,17 @@
 
 #include "chip.h"
 
+#define WWDT_ON
+
 const uint32_t ExtRateIn = 0;
 const uint32_t OscRateIn = 0;
 
 /* SYSTEM SEQUENCE
  * POWER ON -> INIT STATE
- * 		WAIT FOR PIR TO STABALIZE
  * 		CHECK IF IN TEST MODE
  * 		CHECK WATER LEVEL (ENTER ERROR IF LOW)
- * 		TEST PUMP AND LIGHTS (1 BLINK = NORMAL, 2 = TEST)
+ * 		TEST PUMP AND LIGHTS (TOP = TESTING PUMP, BOTTOM = REMAINING PIR STAB TIME)
+ * 		WAIT FOR PIR TO STABALIZE
  * INIT -> CHECK FOR MOVEMENT STATE
  * 		CHECK WATER LEVEL (ENTER ERROR IF LOW)
  * 		CHECK FOR MOVEMENT
@@ -34,12 +36,19 @@ const uint32_t OscRateIn = 0;
  * 		DELAY HYSTERESIS (SHORT DELAY IF TEST)
  * STOP -> CHECK FOR MOVEMENT
  * * -> ERROR
- * 		BLINK LEDs
+ * 		BLINK BOTH LEDs
  * ERROR -> CHECK FOR MOVEMENT
  *
  */
 
-#define WWDT
+//PIN MAPPING (NO FUNCTIONS DEFINED IN SWM)
+#define INPUT_PIR 0
+#define INPUT_TESTPIN 4
+#define INPUT_FLOAT 6
+#define OUTPUT_DEBUG_SYSTICK 7
+#define OUTPUT_LED_TOP 10
+#define OUTPUT_LED_BOTTOM 11
+#define OUTPUT_PUMP_CTRL 13
 
 //OPERATIONAL TIMES TICKED BY 1ms SYSTEM TICK
 #define PIR_STABILIZE_SEC (10*1000)
@@ -51,71 +60,117 @@ const uint32_t OscRateIn = 0;
 #define ON_TIME_BOTTOMLED_SEC (5*1000)
 #define ON_TIME_BOTHLED_SEC (5*1000)
 #define ON_TIME_INIT_SEC (5*1000)
-#define ON_TIME_ERROR_BLINK 10
-#define ON_TIME_ERROR_BLINK_FX_MS (500*1000)
+#define ON_TIME_ERROR_BLINK_SEC (5*1000)
 
 //GP MACROS
-#define TIME_TO_CHECK_STATE (sysTick - stateTick >= STATE_CHECK_MS)
-#define TIME_TO_TURN_OFF (sysTick - waterfallRunning[STARTWFALL].savedTime >= waterfallRunning[STARTWFALL].timeOn)
+#define TIME_TO_CHECK_STATE (ticksNow - lastStateCheck >= STATE_CHECK_MS)
+#define TIME_TO_TURN_OFF (ticksNow - waterfallOpMode[STARTWFALL].savedTime >= waterfallOpMode[STARTWFALL].timeOn)
+#define RUN_WATERFALL_OPERATION (waterfallOpMode[runState].p_RunStateFunction(runState, &waterfallOpMode[runState]))
+#define TURN_TOP_LED_ON (Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_LED_TOP))
+#define TURN_TOP_LED_OFF (Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_LED_TOP))
+#define TURN_BOTTOM_LED_ON (Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM))
+#define TURN_BOTTOM_LED_OFF (Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM))
+#define TURN_BOTH_LED_ON (Chip_GPIO_SetPortOutHigh(LPC_GPIO_PORT, 0, (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM)))
+#define TURN_BOTH_LED_OFF (Chip_GPIO_SetPortOutLow(LPC_GPIO_PORT, 0, (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM)))
+#define TURN_PUMP_ON (Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_PUMP_CTRL))
+#define TURN_PUMP_OFF (Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_PUMP_CTRL))
+#define TURN_ALL_OFF (Chip_GPIO_SetPortOutLow(LPC_GPIO_PORT, 0, (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM) | (1 << OUTPUT_PUMP_CTRL)))
 
 //PROGRAM FLOW TIMING
 #define STATE_CHECK_MS 10
 
-//PIN MAPPING (NO FUNCTIONS DEFINED IN SWM)
-#define INPUT_PIR 0
-#define INPUT_TESTPIN 4
-#define INPUT_FLOAT 6
-#define OUTPUT_DEBUG_SYSTICK 7
-#define OUTPUT_LED_TOP 10
-#define OUTPUT_LED_BOTTOM 11
-#define OUTPUT_PUMP_CTRL 13
-
 //GLOBALS
-static volatile uint32_t sysTick;
+static volatile uint32_t ticksNow;
 uint32_t RUNTIME, HYSTERISIS_TIME; //HOLDS TIMES BASED ON TEST MODE
-uint8_t IS_TEST; //REDUCES TIME FOR T/S IF INPUT_TESTPIN IS LOW ON POWERUP/RESET
 uint8_t waterfallIsRunning;
+enum {INIT, RUNNING, TURN_OFF_TIME};
+typedef enum {TOP, BOTTOM, BOTH} LED_T;
+typedef enum {STARTWFALL=0, TOPLED, BOTTOMLED, BOTHLED, OFF} runState_T;
 
 struct runIt_S
 {
-	uint8_t amIrunning;
+	uint8_t mode;
 	uint32_t savedTime;
 	uint32_t timeOn;
-	void (*p_RunStateFunction)(struct runIt_S *);
+	void (*p_RunStateFunction)(LED_T LED, struct runIt_S *);
 };
 
 //STATE FUNCTION PROTOTYPES
 void init();
-void checkMovement();// __attribute__((optimize(1)));
-void runWaterfall(struct runIt_S *);
-void topLED_run(struct runIt_S *);
-void bottomLED_run(struct runIt_S *);
-void bothLED_run(struct runIt_S *);
+void checkMovement();
+void runWaterfall(LED_T LED, struct runIt_S *);
+void LED_run(LED_T LED, struct runIt_S *);
 void stop();
 void error();
-
 void (*p_StateFunction)() = NULL;
 
 //WORKER FUNCTION PROTOTYPES
-uint8_t checkFloat();
-uint8_t checkPIR();
+uint8_t blinkLED(LED_T whichLED, uint32_t blinkDelay, uint8_t ignoreWaterLevel);
 void setupPort();
+static inline uint8_t waterLevelGood() __attribute__((always_inline));
+static inline uint8_t movementDetected() __attribute__((always_inline));
+static inline uint8_t checkIfTestMode() __attribute__((always_inline));
 
 int main(void) {
-	SystemCoreClockUpdate();
+	//SystemCoreClockUpdate();  //DON'T KNOW IF I REALLY NEED TO CALL THIS
 	SysTick_Config(SystemCoreClock / 1000);
 
 	setupPort();
 
-	typedef enum {STARTWFALL=0, TOPLED, BOTTOMLED, BOTHLED, OFF} runState_T;
 	runState_T runState = STARTWFALL;
 
-	struct runIt_S waterfallRunning[4] = {{0, 0, RUNTIME, &runWaterfall}, {0, 0, ON_TIME_TOPLED_SEC, &topLED_run}, {0, 0, ON_TIME_BOTTOMLED_SEC, &bottomLED_run}, {0, 0, ON_TIME_BOTHLED_SEC, &bothLED_run}};
+	struct runIt_S waterfallOpMode[4] = {{0, 0, RUNTIME, &runWaterfall}, {0, 0, ON_TIME_TOPLED_SEC, &LED_run}, {0, 0, ON_TIME_BOTTOMLED_SEC, &LED_run}, {0, 0, ON_TIME_BOTHLED_SEC, &LED_run}};
 
-	uint32_t stateTick = sysTick;
+	uint32_t lastStateCheck = ticksNow;
 	p_StateFunction = &init;
 
-#ifdef WWDT
+	while(1) {
+		if (TIME_TO_CHECK_STATE)
+		{
+			if (!waterLevelGood())
+			{
+				p_StateFunction = &error;
+				waterfallOpMode[runState].mode = INIT;
+				waterfallIsRunning = FALSE;
+			}
+			else if (waterfallIsRunning)
+			{
+				if (waterfallOpMode[runState].mode == TIME_TO_TURN_OFF)
+				{
+					waterfallOpMode[runState].mode = INIT;
+					runState = runState < OFF ? runState + 1 : TOPLED;
+				}
+				if (movementDetected()) waterfallOpMode[STARTWFALL].timeOn = ticksNow;
+				if (TIME_TO_TURN_OFF)
+				{
+					runState = STARTWFALL;
+					waterfallIsRunning = FALSE;
+					p_StateFunction = &stop;
+				}
+				else RUN_WATERFALL_OPERATION;
+			}
+			else p_StateFunction();
+			lastStateCheck = ticksNow;
+			Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, 0, 7);
+#ifdef WWDT_ON
+			Chip_WWDT_Feed(LPC_WWDT);
+#endif
+		}
+	}
+	return 0;
+}
+
+void setupPort()
+{
+	Chip_GPIO_Init(LPC_GPIO_PORT);
+
+	Chip_IOCON_PinSetMode(LPC_IOCON, IOCON_PIO0, PIN_MODE_PULLDN);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_DEBUG_SYSTICK);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_LED_TOP);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_PUMP_CTRL);
+
+#ifdef WWDT_ON
 	uint32_t wdtFreq;
 	Chip_Clock_SetWDTOSC(WDTLFO_OSC_0_60, 64);
 	Chip_SYSCTL_PowerUp(SYSCTL_SLPWAKE_WDTOSC_PD);
@@ -125,104 +180,19 @@ int main(void) {
 	Chip_WWDT_SetOption(LPC_WWDT, WWDT_WDMOD_WDRESET);
 	Chip_WWDT_Start(LPC_WWDT);
 #endif
-
-	while(1) {
-		if (TIME_TO_CHECK_STATE)
-		{
-#ifdef WWDT
-			Chip_WWDT_Feed(LPC_WWDT);
-#endif
-			if (!checkFloat())
-			{
-				p_StateFunction = &error;
-				waterfallRunning[runState].amIrunning = 0;
-				waterfallIsRunning = 0;
-			}
-			else if (waterfallIsRunning)
-			{
-				if (waterfallRunning[runState].amIrunning == 2)
-				{
-					waterfallRunning[runState].amIrunning = 0;
-					runState = runState < OFF ? runState + 1 : TOPLED;
-				}
-				if (checkPIR()) waterfallRunning[STARTWFALL].timeOn = sysTick;
-				if (TIME_TO_TURN_OFF)
-				{
-					runState = STARTWFALL;
-					waterfallIsRunning = 0;
-					p_StateFunction = &stop;
-				}
-				else waterfallRunning[runState].p_RunStateFunction(&waterfallRunning[runState]);
-			}
-			else p_StateFunction();
-			stateTick = sysTick;
-			Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, 0, 7);
-		}
-	}
-	return 0;
-}
-
-void setupPort()
-{
-	/* Initialize GPIO */
-	Chip_GPIO_Init(LPC_GPIO_PORT);
-
-	//PIO0_0 - INPUT, PD_ON :: PIR INPUT
-	Chip_IOCON_PinSetMode(LPC_IOCON, IOCON_PIO0, PIN_MODE_PULLDN);
-	//PIO0_4 - INPUT, PU_ON :: TEST MODE INPUT
-	//PIO0_6 - INPUT, PU_ON :: FLOAT INPUT
-	//PIO0_7 - OUTPUT :: SYSTICK TOGGLE FOR DEBUG
-	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_DEBUG_SYSTICK);
-	//PIO0_10 - OUTPUT (o/c) :: LED_TOP
-	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_LED_TOP);
-	//PIO0_11 - OUTPUT (o/c) :: LEDBOTTOM
-	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
-	//PIO0_13 - OUTPUT :: PUMP CONTROL
-	Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, OUTPUT_PUMP_CTRL);
-	//PIO0_1 - DEFAULT :: UNUSED
-	//PIO0_2 - DEFAULT :: SWDIO
-	//PIO0_3 - DEFAULT :: SWCLK
-	//PIO0_4 - DEFAULT :: UNUSED
-	//PIO0_5 - DEFAULT :: RESET
-	//PIO0_8 - DEFAULT :: UNUSED
-	//PIO0_9 - DEFAULT :: UNUSED
-	//PIO0_12 - DEFAULT :: UNUSED (ISP p/u)
 }
 
 void init()
 {
-	uint32_t nowTime = sysTick;
-	uint8_t run2xIfTestMode = 0;
+	uint8_t IS_TEST = checkIfTestMode() ? TRUE : FALSE;
 
-	Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
-	while (sysTick - nowTime <= PIR_STABILIZE_SEC)
+	if (waterLevelGood())
 	{
-#ifdef WWDT
-		Chip_WWDT_Feed(LPC_WWDT);
-#endif
-	}
-	Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
+			TURN_PUMP_ON;
+			blinkLED(TOP, ON_TIME_INIT_SEC, FALSE);
+			TURN_PUMP_OFF;
 
-	IS_TEST = !Chip_GPIO_GetPinState(LPC_GPIO_PORT, 0, INPUT_TESTPIN) ? 1 : 0;
-
-	if (checkFloat()) //level good
-	{
-		Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_PUMP_CTRL);
-		for (run2xIfTestMode = 0; run2xIfTestMode < IS_TEST+1; run2xIfTestMode++)
-		{
-			nowTime = sysTick;
-			Chip_GPIO_SetPortOutHigh(LPC_GPIO_PORT, 0, (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM));
-			while (sysTick - nowTime <= ON_TIME_INIT_SEC)
-			{
-				if (!checkFloat()) break;
-#ifdef WWDT
-				Chip_WWDT_Feed(LPC_WWDT);
-#endif
-			}
-			Chip_GPIO_SetPortOutLow(LPC_GPIO_PORT, 0, (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM));
-		}
-		Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_PUMP_CTRL);
-		p_StateFunction = &checkMovement;
+			if (!blinkLED(BOTTOM, PIR_STABILIZE_SEC-ON_TIME_INIT_SEC, FALSE)) p_StateFunction = &error;
 	}
 	else p_StateFunction = &error;
 
@@ -232,80 +202,47 @@ void init()
 
 void checkMovement()
 {
-	if (checkPIR()) waterfallIsRunning = 1;
+	if (movementDetected()) waterfallIsRunning = TRUE;
 }
 
-void runWaterfall(struct runIt_S *run)
+void runWaterfall(LED_T LED, struct runIt_S *run)
 {
-	run->amIrunning = 2;
-	run->savedTime = sysTick;
+	run->mode = TURN_OFF_TIME;
+	run->savedTime = ticksNow;
 	run->timeOn = RUNTIME;
-	Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_PUMP_CTRL);
+	TURN_PUMP_ON;
 }
 
-void topLED_run(struct runIt_S *run)
+void LED_run(LED_T LED, struct runIt_S *run)
 {
-	if (run->amIrunning)
+	if (run->mode)
 	{
-		if (sysTick - run->savedTime >= run->timeOn)
+		if (ticksNow - run->savedTime >= run->timeOn)
 		{
-			Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_LED_TOP);
-			run->amIrunning = 2;
+			TURN_BOTH_LED_OFF;
+			run->mode = TURN_OFF_TIME;
 		}
 	}
 	else
 	{
-		run->amIrunning = 1;
-		run->savedTime = sysTick;
-		Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_LED_TOP);
-	}
-}
-
-void bottomLED_run(struct runIt_S *run)
-{
-	if (run->amIrunning)
-	{
-		if (sysTick - run->savedTime >= run->timeOn)
-		{
-			Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
-			run->amIrunning = 2;
-		}
-	}
-	else
-	{
-		run->amIrunning = 1;
-		run->savedTime = sysTick;
-		Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
-	}
-}
-
-void bothLED_run(struct runIt_S *run)
-{
-	if (run->amIrunning)
-	{
-		if (sysTick - run->savedTime >= run->timeOn)
-		{
-			Chip_GPIO_SetPortOutLow(LPC_GPIO_PORT, 0, (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM));
-			run->amIrunning = 2;
-		}
-	}
-	else
-	{
-		run->amIrunning = 1;
-		run->savedTime = sysTick;
-		Chip_GPIO_SetPortOutHigh(LPC_GPIO_PORT, 0, (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM));
+		run->mode = RUNNING;
+		run->savedTime = ticksNow;
+		if (LED == TOP) TURN_TOP_LED_ON;
+		else if (LED == BOTTOM) TURN_BOTTOM_LED_ON;
+		else TURN_BOTH_LED_ON;
 	}
 }
 
 void stop()
 {
-	waterfallIsRunning = 0;
-	uint32_t nowTime = sysTick;
-	Chip_GPIO_SetPortOutLow(LPC_GPIO_PORT, 0, (1 << OUTPUT_PUMP_CTRL) | (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM));
-	while (sysTick - nowTime <= HYSTERISIS_TIME)
+	waterfallIsRunning = FALSE;
+	uint32_t nowTime = ticksNow;
+	TURN_ALL_OFF;
+
+	while (ticksNow - nowTime <= HYSTERISIS_TIME)
 	{
-		if (!checkFloat()) break;
-#ifdef WWDT
+		if (!waterLevelGood()) break;
+#ifdef WWDT_ON
 		Chip_WWDT_Feed(LPC_WWDT);
 #endif
 	}
@@ -314,47 +251,52 @@ void stop()
 
 void error()
 {
-	waterfallIsRunning = 0;
-	Chip_GPIO_SetPortOutLow(LPC_GPIO_PORT, 0, (1 << OUTPUT_PUMP_CTRL) | (1 << OUTPUT_LED_TOP) | (1 << OUTPUT_LED_BOTTOM));
+	waterfallIsRunning = FALSE;
+	TURN_ALL_OFF;
 
-	uint8_t blinkLoop = 0;
-	uint32_t storeTime = 0;
+	blinkLED(BOTH, ON_TIME_ERROR_BLINK_SEC, TRUE);
 
-	for (blinkLoop = 0; blinkLoop < ON_TIME_ERROR_BLINK; blinkLoop++)
-	{
-		Chip_GPIO_SetPinOutHigh(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
-		storeTime = sysTick;
-		while (sysTick - storeTime <= (ON_TIME_ERROR_BLINK_FX_MS/2))
-		{
-			//__NOP;
-#ifdef WWDT
-			Chip_WWDT_Feed(LPC_WWDT);
-#endif
-		}
-		Chip_GPIO_SetPinOutLow(LPC_GPIO_PORT, 0, OUTPUT_LED_BOTTOM);
-		storeTime = sysTick;
-		while (sysTick - storeTime <= (ON_TIME_ERROR_BLINK_FX_MS/2))
-		{
-			//__NOP;
-#ifdef WWDT
-			Chip_WWDT_Feed(LPC_WWDT);
-#endif
-		}
-	}
 	p_StateFunction = &checkMovement;
 }
 
-uint8_t checkFloat()
+uint8_t blinkLED(LED_T whichLED, uint32_t blinkDelay, uint8_t ignoreWaterLevel)
+{
+	uint32_t ticksThen = ticksNow;
+
+	if (whichLED == BOTH) TURN_BOTH_LED_ON;
+	else if (whichLED == TOP) TURN_TOP_LED_ON;
+	else TURN_BOTTOM_LED_ON;
+
+	while (ticksNow - ticksThen <= blinkDelay)
+	{
+		//__NOP;
+#ifdef WWDT_ON
+		Chip_WWDT_Feed(LPC_WWDT);
+#endif
+		if (ignoreWaterLevel && !waterLevelGood()) return FALSE;
+	}
+
+	TURN_BOTH_LED_OFF;
+
+	return TRUE;
+}
+
+uint8_t waterLevelGood()
 {
 	return !Chip_GPIO_GetPinState(LPC_GPIO_PORT, 0, INPUT_FLOAT);
 }
 
-uint8_t checkPIR()
+uint8_t movementDetected()
 {
 	return Chip_GPIO_GetPinState(LPC_GPIO_PORT, 0, INPUT_PIR);
 }
 
+uint8_t checkIfTestMode()
+{
+	return !Chip_GPIO_GetPinState(LPC_GPIO_PORT, 0, INPUT_TESTPIN);
+}
+
 void SysTick_Handler(void)
 {
-	sysTick++;
+	ticksNow++;
 }
